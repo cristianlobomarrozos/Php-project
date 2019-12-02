8f9f12c7820a48c0fb9f5eeaa629e45a31e0e2b8
Lobo <cristian.lobo.marrozos@gmail.com>
2019-12-02 11:48:53 +0100

    Changes

--- Coches/index.php
+++ Coches/index.php
@@ -6,12 +6,7 @@
 		<div class="col-md-12">
 			
 		</div>
-<<<<<<< HEAD
 	</div>	<div class="row">
-=======
-	</div>
-	<div class="row">
->>>>>>> master
 		<div class="col-md-6">
 			<h2>
 				Modernos
